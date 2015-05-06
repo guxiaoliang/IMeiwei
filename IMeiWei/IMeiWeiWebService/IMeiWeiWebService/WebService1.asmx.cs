fe73628c254b047a9f2396803ee4afeb38da4d9d
guxiaoliang <guxiaoliang0208@126.com>
2015-05-06 21:47:14 +0800

    Add pay interface.

--- IMeiWei/IMeiWeiWebService/IMeiWeiWebService/WebService1.asmx.cs
+++ IMeiWei/IMeiWeiWebService/IMeiWeiWebService/WebService1.asmx.cs
@@ -27,7 +27,7 @@ namespace IMeiWeiWebService
 
         [WebMethod]
         [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
-        public string Registration(string phoneNumber, string password, string securityNumber,string userName)
+        public string Registration(string phoneNumber, string password, string securityNumber, string userName)
         {
             IMeiWei.BLL.user bll_user = new IMeiWei.BLL.user();
             IMeiWei.Model.user model_user = new IMeiWei.Model.user();
@@ -48,6 +48,58 @@ namespace IMeiWeiWebService
             model_user.PhoneNumber = phoneNumber;
 
             bll_user.Add(model_user);
+
+            return "SUCCESS";
+        }
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public string Login(string phoneNumber, string password)
+        {
+            IMeiWei.BLL.user bll_user = new IMeiWei.BLL.user();
+            IMeiWei.Model.user model_user = new IMeiWei.Model.user();
+
+            model_user = bll_user.GetModelList(" phoneNumber = '" + phoneNumber + "'").FirstOrDefault();
+
+            if (null != model_user)
+            {
+                if (model_user.Password != password)
+                {
+                    return "PASSWORD_ERROR";
+                }
+                else
+                {
+                    string tokenId = Guid.NewGuid().ToString();
+                    Session.Add(tokenId, phoneNumber);
+                    return tokenId;
+                }
+            }
+            else
+            {
+                return "USERNAME_ERROR";
+            }
+        }
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public string CreateBusiness(
+            string businessId, string businessName, string businessPhone,string businessHours, 
+            int? percapitaconsumption, int? restauranthighLights, string firstOffer,
+            string vipOffer, string businessTypeid)
+        {
+            IMeiWei.BLL.business bll_business = new IMeiWei.BLL.business();
+            IMeiWei.Model.business model_business = new IMeiWei.Model.business();
+
+            model_business.BusinessId = businessId;
+            model_business.BusinessName = businessName;
+            model_business.BusinessPhone = businessPhone;
+            model_business.BusinessTypeId = businessTypeid;
+            model_business.FirstOffer = firstOffer;
+            model_business.PerCapitaConsumption = percapitaconsumption;
+            model_business.VIPOffer = vipOffer;
+            model_business.PerCapitaConsumption = restauranthighLights;
+
+            bll_business.Add(model_business);
 
             return "SUCCESS";
         }
